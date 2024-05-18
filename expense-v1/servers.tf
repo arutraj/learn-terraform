5938dbd76ac17992acda41c11c689f8fe845348c
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-18 08:21:04 +0530

    Learn Terraform

--- expense-v1/servers.tf
+++ expense-v1/servers.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0a1a60a833ad42c5f"]
 
   tags = {
     Name = "frontend"
@@ -10,6 +11,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "backend" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0a1a60a833ad42c5f"]
 
   tags = {
     Name = "backend"
@@ -19,6 +21,7 @@ resource "aws_instance" "backend" {
 resource "aws_instance" "mysql" {
   ami           = "ami-090252cbe067a9e58"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0a1a60a833ad42c5f"]
 
   tags = {
     Name = "mysql"
