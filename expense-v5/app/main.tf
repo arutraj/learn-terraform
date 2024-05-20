37ed4e3d9494a782ccc60e5cf9e0a86cc7c9a0c1
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-20 08:53:42 +0530

    Learn Terraform

--- expense-v5/app/main.tf
+++ expense-v5/app/main.tf
@@ -6,6 +6,11 @@ resource "aws_instance" "node" {
   tags = {
     Name = var.name
   }
+
+  provisioner "local-exec" {
+    command = "sleep 120; cd /home/ec2-user/expense-ansible ; ansible-playbook -i ${self.private_ip}, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${var.name} -e env=dev expense.yml"
+  }
+
 }
 
 resource "aws_route53_record" "record" {
