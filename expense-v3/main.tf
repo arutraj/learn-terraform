5fd958825b502a20079bb402b8ca85151bf54bc7
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-20 07:55:34 +0530

    Learn Terraform

--- expense-v3/main.tf
+++ expense-v3/main.tf
@@ -23,3 +23,9 @@ variable "components" {
   }
 }
 
+# frontend = {
+#   instance_type = "t3.small"
+# }
+
+# In the above input key is frontend, value is { instance_type = "t3.small" }.
+# since it is been called by for_each, each prefix is needed like each.key and each.value
